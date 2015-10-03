427aea5a7bc414fc1b49c70bcc7b26e59985d939
girish <girish946@gmail.com>
2015-10-04 00:16:31 +0530

    updated

--- yowsup/__init__.py
+++ yowsup/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "2.3.84"
+__version__ = "2.4"
 __author__ = "Tarek Galal"
--- yowsup/env/env_s40.py
+++ yowsup/env/env_s40.py
@@ -2,12 +2,12 @@ from .env import YowsupEnv
 import base64
 import hashlib
 class S40YowsupEnv(YowsupEnv):
-    _VERSION = "2.12.82"
+    _VERSION = "2.13.5"
     _OS_NAME= "S40"
     _OS_VERSION = "14.26"
     _DEVICE_NAME = "Nokia302"
-    _TOKEN_STRING  = "PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1431039885607{phone}"
-    _AXOLOTL = False
+    _TOKEN_STRING  = "PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1442277896714{phone}"
+    _AXOLOTL = True
 
     def getVersion(self):
         return self.__class__._VERSION
@@ -34,4 +34,3 @@ class S40YowsupEnv(YowsupEnv):
             OS_VERSION = self.getOSVersion(),
             DEVICE_NAME = self.getDeviceName()
         )
-
